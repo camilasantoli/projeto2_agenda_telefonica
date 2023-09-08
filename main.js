5edf2d7499bbd129f07608e8829ca5072fb1af13
Camila Santana <camila.santoli@hotmail.com>
2023-09-08 18:35:51 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('formulario');
+const nomes = []
+const telefones = []
+
+linhas = ''
+
+form;addEventListener('submit', function(e){
+    e.preventDefault();
+
+    adicionarLinha()
+    atualizaTabela ()
+})
+
+function adicionarLinha(){
+    const inputNome = document.getElementById('nome')
+    const inputTelefone = document.getElementById('telefone')
+
+    if (nomes.includes(inputNome.value)) {
+        alert(`O nome ${inputNome.value} já foi inserido`)
+    } else {
+        nomes.push (inputNome.value)
+        telefones.push(Number(inputNome.value))
+    }
+
+    let linha = `<tr>`
+    linha += `<td>${inputNome.value}</td>`
+    linha += `<td>${inputTelefone.value}</td>`
+    linha += `</tr>`
+
+    linhas += linha
+
+    inputNome.value = ''
+    inputTelefone.value = ''
+}
+
+function atualizaTabela (){
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas
+}
